18951aee9b115263272b4e8d44bffaa86f1b2587
Brian Matamet <patrickmat101@gmail.com>
2020-10-28 22:28:43 -0700

    Factorial 2.0

--- 00_Factorial.cpp
+++ 00_Factorial.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 // #include 
 
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    cout << "Factorial of " << n << "is: " << factorial(n);
+}
+
+//Solves for the factorial of any input (n)
+int factorial(int n)
+{
+    //Recursion case
+    if(n > 1)
+    {
+        //Recursion happens here
+        return n * factorial(n-1);
+    }
+    //Base case
+    else
+        return 1;
+}
